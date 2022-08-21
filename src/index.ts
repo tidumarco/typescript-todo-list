eacb587297780454ab125f4ab111527c8be2078f
Marco Tidu <matidu@utu.fi>
2022-08-21 11:37:18 +0200

    Added local storage functionality

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@ type Task = {
 const list = document.querySelector<HTMLUListElement>("#list");
 const form = document.querySelector<HTMLFormElement>("#new-task-form");
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem)
 
 form?.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -24,16 +26,34 @@ form?.addEventListener("submit", (e) => {
     completed: false,
     createdAt: new Date(),
   };
+  tasks.push(newTask);
 
   addListItem(newTask);
+  input.value = "";
 });
 
 function addListItem(task: Task) {
   const item = document.createElement("li");
   const label = document.createElement("label");
   const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+    console.log(tasks);
+  });
   checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
   label.append(checkbox, task.title);
   item.append(label);
   list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem('TASKS')
+  if(taskJSON == null) return []
+  return JSON.parse(taskJSON)
 }
